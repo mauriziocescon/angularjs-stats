b6605c271773c7e61622420174b15bcdb7fe3d1e
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-03-19 12:57:18 +0100

    Interface improvement

--- angular-stats.d.ts
+++ angular-stats.d.ts
@@ -1,45 +1,21 @@
 /// <reference types="angular" />
 
-import * as angular from "angular";
+export = mc;
 
-declare module "angular" {
-	export namespace mc {
+declare namespace mc {
 
-		interface IAngularStats {
-			/**
-			 * Set the element from where the
-			 * analysis will start
-			 *
-			 * @param element
-			 */
-			setStartingElement(element: any): void;
-			/**
-			 * Count the number of scopes / watchers
-			 * for every component. Analyze the DOM
-			 */
-			analyzeWebApp: () => string;
-		}
+	interface IAngularStats {
+		/**
+		 * Set the element from where the
+		 * analysis will start
+		 *
+		 * @param element
+		 */
+		setStartingElement(element: any): void;
+		/**
+		 * Count the number of scopes / watchers
+		 * for every component. Analyze the DOM
+		 */
+		analyzeWebApp: () => string;
 	}
 }
-
-/// <reference types="angular" />
-
-// export = mc;
-//
-// declare namespace mc {
-//
-// 	interface IAngularStats {
-// 		/**
-// 		 * Set the element from where the
-// 		 * analysis will start
-// 		 *
-// 		 * @param element
-// 		 */
-// 		setStartingElement(element: any): void;
-// 		/**
-// 		 * Count the number of scopes / watchers
-// 		 * for every component. Analyze the DOM
-// 		 */
-// 		analyzeWebApp: () => string;
-// 	}
-// }
