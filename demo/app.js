638f950b7386c9e992821e8bdff27e4064314e76
Maurizio Cescon <Maurizio.CESCON@ext.ec.europa.eu>
2017-09-19 12:10:10 +0200

    minor changes

--- demo/app.js
+++ demo/app.js
@@ -1,18 +1,18 @@
 angular.module("demo", ["angular-stats"])
-    .controller("MainCtrl", ["$scope", function ($scope) {
+    .controller("MainCtrl", ["$scope", function($scope) {
         $scope.name = "MainCtrl";
         $scope.values = [0];
 
-        $scope.clickButton = function () {
+        $scope.clickButton = function() {
             var value = new Date().getTime();
             $scope.values.push(value);
         };
     }])
-    .controller("StatCtrl", ["$scope", "$document", "AngularStats", function ($scope, $document, AngularStats) {
+    .controller("StatCtrl", ["$scope", "$document", "AngularStats", function($scope, $document, AngularStats) {
         $scope.name = "StatCtrl";
         AngularStats.setStartingElement($document[0].querySelector("div[ng-app]"));
 
-        $scope.getStats = function() {
+        $scope.getStats = function(){
             return AngularStats.analyzeWebApp();
-        }
+        };
     }]);
