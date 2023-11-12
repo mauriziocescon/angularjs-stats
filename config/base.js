e5a0249ce38d7c425599bb20bba7bad0af48c51c
Maurizio Cescon <maurizio.cescon@gmail.com>
2023-11-12 14:00:15 +0100

    chore: source-map

--- config/base.js
+++ config/base.js
@@ -8,6 +8,8 @@ module.exports = () => {
 
     entry: './src/index.ts',
 
+    devtool: 'inline-source-map',
+
     resolve: {
       // Add '.ts' and '.tsx' as a resolvable extension.
       extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.scss', '.html', '.json'],
